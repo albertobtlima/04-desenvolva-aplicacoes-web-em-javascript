9082c85c927e81eaa261d25cb97ee5cca7e283dd
Alberto Lima <alberto.btlima@outlook.com>
2025-03-25 21:00:18 -0300

    add: main.js

--- 01-javascript-manipulando-objetos/main.js
+++ 01-javascript-manipulando-objetos/main.js
@@ -1,1 +1,54 @@
-let listaDeCompras = ["Arroz", "Feijão", "Suco"];
+let listaDeItens = [];
+
+const form = document.getElementById("form-itens");
+const itensInput = document.getElementById("receber-item");
+const ulItens = document.getElementById("lista-de-itens");
+
+form.addEventListener("submit", function (evento) {
+  evento.preventDefault();
+  salvarItem();
+  mostrerItem();
+});
+
+function salvarItem() {
+  const comprasItem = itensInput.value;
+  const checarDuplicado = listaDeItens.some(
+    (elemento) => elemento.valor.toUpperCase() === comprasItem.toUpperCase()
+  );
+
+  if (checarDuplicado) {
+    alert("Item ja adicionado");
+  } else {
+    listaDeItens.push({
+      valor: comprasItem,
+    });
+  }
+
+  console.log(listaDeItens);
+}
+
+function mostrerItem() {
+  ulItens.innerHTML = "";
+
+  listaDeItens.forEach((elemento, index) => {
+    ulItens.innerHTML += `
+      <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+          <input type="checkbox" class="is-clickable" />
+          <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+        </div>
+        <div>
+          <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+      </li>
+    `;
+  });
+
+  const inputsCheck = document.querySelectorAll("input[type='checkbox']");
+
+  inputsCheck.forEach((i) => {
+    i.addEventListener("click", (evento) => {
+      console.log("Fui clicado");
+    });
+  });
+}
