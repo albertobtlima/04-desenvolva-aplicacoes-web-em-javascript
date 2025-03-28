b0ba198cf6afc136413dc5ae6858156f99186ab8
Alberto Lima <alberto.btlima@outlook.com>
2025-03-28 17:18:37 -0300

    add salvar no localStorage

--- 01-javascript-manipulando-objetos/main.js
+++ 01-javascript-manipulando-objetos/main.js
@@ -5,6 +5,18 @@ const form = document.getElementById("form-itens");
 const itensInput = document.getElementById("receber-item");
 const ulItens = document.getElementById("lista-de-itens");
 const ulItensComprados = document.getElementById("itens-comprados");
+const listaRecuperada = localStorage.getItem("listaDeItens");
+
+function atualizaLocalStorage() {
+  localStorage.setItem("listaDeItens", JSON.stringify(listaDeItens));
+}
+
+if (listaRecuperada) {
+  listaDeItens = JSON.parse(listaRecuperada);
+  mostrarItem();
+} else {
+  listaDeItens = [];
+}
 
 form.addEventListener("submit", function (evento) {
   evento.preventDefault();
@@ -47,29 +59,27 @@ function mostrarItem() {
             <i class="fa-solid fa-trash is-clickable deletar"></i>
           </div>
         </li>
-        `;
+      `;
     } else {
       ulItens.innerHTML += `
         <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
           <div>
             <input type="checkbox" class="is-clickable" />
-            <input type="text" class="is-size-5" 
-              value="${elemento.valor}" ${
+            <input type="text" class="is-size-5" value="${elemento.valor}" ${
         index !== Number(itemAEditar) ? "disabled" : ""
-      }>
-            </input>
+      }> </input>
           </div>
-
+          </div>
           <div>
             ${
               index === Number(itemAEditar)
                 ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>'
-                : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'
+                : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i> '
             }
             <i class="fa-solid fa-trash is-clickable deletar"></i>
           </div>
         </li>
-    `;
+      `;
     }
   });
 
@@ -104,13 +114,17 @@ function mostrarItem() {
       mostrarItem();
     });
   });
+
+  atualizaLocalStorage();
 }
 
 function salvarEdicao() {
   const itemEditado = document.querySelector(
     `[data-value="${itemAEditar}"] input[type="text"]`
   );
+
   listaDeItens[itemAEditar].valor = itemEditado.value;
+  console.log(listaDeItens);
   itemAEditar = -1;
   mostrarItem();
 }
