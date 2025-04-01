bb9ed0e947b0c975e095d8edb4bcd8af0f182e69
Alberto Lima <alberto.btlima@outlook.com>
2025-04-01 16:43:45 -0300

    add: class guerreiro

--- 02-javascript-classes-e-herancas-no-desenvolvimento-de-aplicacoes/src/modules/personagem.js
+++ 02-javascript-classes-e-herancas-no-desenvolvimento-de-aplicacoes/src/modules/personagem.js
@@ -2,16 +2,20 @@ export class Personagem {
   nome;
   vida = 100;
   mana = 100;
-  level;
+  #level;
   descricao;
 
-  constructor(nome, level) {
+  constructor(nome) {
     this.nome = nome;
-    this.level = level;
+    this.#level = 1;
+  }
+
+  get level() {
+    return this.#level;
   }
 
   obterInsignia() {
-    if (this.level >= 5) {
+    if (this.#level >= 5) {
       return `${this.constructor.tipo} Implacavel`;
     }
 
