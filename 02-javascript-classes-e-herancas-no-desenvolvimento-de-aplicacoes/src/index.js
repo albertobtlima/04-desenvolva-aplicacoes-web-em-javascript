c9f489b258e84c5cb93bde837aabef39047881a0
Alberto Lima <alberto.btlima@outlook.com>
2025-04-01 15:36:31 -0300

    add: class ArqueiroMago

--- 02-javascript-classes-e-herancas-no-desenvolvimento-de-aplicacoes/src/index.js
+++ 02-javascript-classes-e-herancas-no-desenvolvimento-de-aplicacoes/src/index.js
@@ -1,12 +1,13 @@
-import { Personagem } from "./modules/personagem.js";
 import { PersonagemView } from "./components/personagem-view.js";
 import { Mago } from "./modules/mago.js";
 import { Arqueiro } from "./modules/arqueiro.js";
+import { ArqueiroMago } from "./modules/arqueiro-mago.js";
 
 const magoAntonio = new Mago("Antonio", 4, "fogo", 4, 7);
 const magoJulia = new Mago("Julia", 8, "gelo", 7, 10);
 const arqueiroBruno = new Arqueiro("Bruno", 7, 8);
+const arqueiroMagoChico = new ArqueiroMago("Chico", 7, 10, "ar", 4, 8);
 
-const personagens = [magoAntonio, magoJulia, arqueiroBruno];
+const personagens = [magoAntonio, magoJulia, arqueiroBruno, arqueiroMagoChico];
 
 new PersonagemView(personagens).render();
